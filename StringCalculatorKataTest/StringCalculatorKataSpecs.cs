b7edb8abba783d22d13090f4da52b7de8c6d5324
Rufino Perez <ruperez@aidacanarias.com>
2022-05-11 08:06:17 +0100

    Change project type and test class

---
+++ StringCalculatorKataTest/StringCalculatorKataSpecs.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace StringCalculatorKataTest
+{
+    public class StringCalculatorKataSpecs
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
