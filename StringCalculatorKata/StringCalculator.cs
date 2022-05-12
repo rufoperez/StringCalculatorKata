1040d59b4078b7174f56ccb871ee5356d164cfbf
Rufino Perez <ruperez@aidacanarias.com>
2022-05-12 07:54:17 +0100

    refactor

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -6,10 +6,17 @@ public static class StringCalculator
 {
     public static int Add(string value)
     {
-        if(string.IsNullOrEmpty(value))
+        var isOneNumber = value.Length == 1;
+
+        if (string.IsNullOrEmpty(value))
             return 0;
-        if(value.Length == 1)
+        if(isOneNumber)
             return int.Parse(value);
+        return SumStringValues(value);
+    }
+
+    private static int SumStringValues(string value)
+    {
         return int.Parse(value.Split(',')[0]) + int.Parse(value.Split(',')[1]);
     }
 }
