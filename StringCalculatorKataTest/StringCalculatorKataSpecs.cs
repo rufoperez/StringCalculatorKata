7d1270aeddff4933d1c22baba91f555ba7517f1b
Rufino Perez <ruperez@aidacanarias.com>
2022-05-12 07:33:39 +0100

    red when_value_is_1_then_return_1

--- StringCalculatorKataTest/StringCalculatorKataSpecs.cs
+++ StringCalculatorKataTest/StringCalculatorKataSpecs.cs
@@ -16,5 +16,12 @@ namespace StringCalculatorKataTest
             int result = StringCalculator.Add("");
             Assert.AreEqual(0, result);
         }
+
+        [Test]
+        public void when_value_is_1_then_return_1()
+        {
+            int result = StringCalculator.Add("1");
+            Assert.AreEqual(1, result);
+        }
     }
 }
