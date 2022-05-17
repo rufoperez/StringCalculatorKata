b7e30fb39dcabdb3e31da3b5bc7e8379591202a0
Rufino Perez <ruperez@aidacanarias.com>
2022-05-17 07:59:17 +0100

    reset

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -4,33 +4,5 @@ namespace StringCalculatorKata;
 
 public static class StringCalculator
 {
-    public static int Add(string value)
-    {
-        if (string.IsNullOrEmpty(value))
-            return 0;
-        return SumStringValues(value);
-    }
-
-    private static int SumStringValues(string value)
-    {
-        string[] delimeters = GetDelimeters(value);
-
-        if (value.StartsWith("//"))
-        {
-            value = value.Substring(3, value.Length - 3);
-        }
-        
-        int result = 0;
-        List<string> values = value.Split(delimeters, StringSplitOptions.None).ToList();
-        foreach (var stringValue in values)
-            result += int.Parse(stringValue);
-        return result;
-    }
-
-    private static string[] GetDelimeters(string value)
-    {
-        if (value.StartsWith("//"))
-            return new [] { value.Substring(2, value.Length - 2).Split('\n')[0] };
-        return new [] { ",", "\n" };
-    }
+    
 }
